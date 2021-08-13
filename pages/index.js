f34856f54a7606843bff2d159aec381a22117a81
inihery <idabagusheryana60@gmail.com>
2021-08-13 08:29:55 +0800

    revisi#3

--- pages/index.js
+++ pages/index.js
@@ -24,47 +24,88 @@ export default function Home() {
 
   return (
     <div className="body">
+      <h3 className="tittle">To-Do List</h3>
       <div className="app">
-        <h3 className="tittle">To-Do List</h3>
-        <form className="form" onSubmit={(e) => handleSubmit(e)}>
-          <input
-            placeholder="Add your text..."
-            name="value"
-            className="input"
-            onChange={(e) => {
-              setInputValue(e.target.value);
-            }}
-            value={inputValue}
-          />
-          <button type="submit" className="button">
-            <Image
-              width="22px"
-              height="22px"
-              src="/icon/paper-plane.png"
-              alt="add"
+        <div>
+          <div className="flex-item">
+            <div className="icon-left">
+              <Image
+                width="16px"
+                height="16px"
+                src="/icon/left-arrow.png"
+                alt="left"
+              />
+            </div>
+            <div className="day">Monday</div>
+            <div className="icon-rigth">
+              <Image
+                width="16px"
+                height="16px"
+                src="/icon/right-arrow.png"
+                alt="left"
+              />
+            </div>
+          </div>
+          <form className="form" onSubmit={(e) => handleSubmit(e)}>
+            <input
+              className="add"
+              placeholder="Add your text..."
+              name="value"
+              className="input"
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+              value={inputValue}
             />
-          </button>
-        </form>
+            <button type="submit" className="button">
+              <Image width="18px" height="18px" src="/icon/add.png" alt="add" />
+            </button>
+          </form>
+        </div>
 
         {todos.map((todo, index) => {
           return (
-            <div className="todo-item" key={index + "_todo"}>
-              <p className="label">{todo}</p>
-              <button
-                className="delete-button"
-                onClick={() => deleteTodo(index)}
+            <div className="todo-item">
+              <div
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "20px auto 20px",
+                }}
+                key={index + "_todo"}
               >
-                <Image
-                  width="21px"
-                  height="21px"
-                  src="/icon/remove.png"
-                  alt="delete"
-                />
-              </button>
+                <div style={{ width: 18, height: 18, paddingTop: "6px" }}>
+                  <Image
+                    width="20px"
+                    height="20px"
+                    src="/icon/checked.png"
+                    alt="delete"
+                  />
+                </div>
+
+                <div className="label" style={{ padding: "0px 12px" }}>
+                  {todo}
+                </div>
+                <div>
+                  <button
+                    // style={{ width: 30, height: 30 }}
+                    // type="button"
+                    className="delete-button"
+                    onClick={() => deleteTodo(index)}
+                  >
+                    <Image
+                      width="20px"
+                      height="20px"
+                      src="/icon/rubbish.png"
+                      alt="delete"
+                    />
+                  </button>
+                </div>
+              </div>
             </div>
           );
         })}
       </div>
+      <div>Clear All</div>
     </div>
   );
 }
