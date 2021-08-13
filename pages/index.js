3c7fef08b4b938e0a687d4782501d5cf89a4b510
inihery <idabagusheryana60@gmail.com>
2021-08-13 22:01:49 +0800

    revisi#5

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,19 @@ import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
 
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 export default function Home() {
   const [todos, setTodos] = useState([]);
+  const [selectedDayIndex, setSelectedDayIndex] = useState(1);
 
   const [inputValue, setInputValue] = useState("");
 
@@ -11,7 +22,7 @@ export default function Home() {
     event.preventDefault();
 
     const newTodos = [...todos];
-    newTodos.push(inputValue);
+    newTodos.push({ task: inputValue, isDone: false });
     setTodos(newTodos);
     setInputValue("");
   };
@@ -23,37 +34,70 @@ export default function Home() {
     console.log(newTodos);
   };
 
+  // const deleteTodo = (index) => {
+  //   const newTodos = [...todos];
+  //   newTodos.splice(index, 1);
+  //   setTodos("");
+  // };
+
+  const onNextDay = (currentIndex) => {
+    currentIndex = currentIndex + 1;
+    if (currentIndex === 7) {
+      currentIndex = 0;
+    }
+    setSelectedDayIndex(currentIndex);
+  };
+
+  const onPrevDay = (currentIndex) => {
+    currentIndex = currentIndex - 1;
+    if (currentIndex === 0) {
+      currentIndex = 6;
+    }
+    setSelectedDayIndex(currentIndex);
+  };
+
+  const onToogleCompletion = (index) => {
+    const newTodos = [...todos];
+    const selectedTodo = { ...newTodos[index] };
+    newTodos[index] = { task: selectedTodo.task, isDone: !selectedTodo.isDone };
+
+    setTodos(newTodos);
+  };
+
+  const onClearTodos = () => {
+    setTodos([]);
+  };
+
   return (
     <div className="body">
       <h3 className="tittle">ToDoList</h3>
       <div className="app">
         <div>
           <div className="flex-item">
-            <div className="icon-left">
-              <Link href="/">
-                <a>
-                  <Image
-                    width="16px"
-                    height="16px"
-                    src="/icon/left-arrow.png"
-                    alt="left"
-                  />
-                </a>
-              </Link>
-            </div>
-            <div className="day">Monday</div>
-            <div className="icon-left">
-              <Link href="TodoList2">
-                <a>
-                  <Image
-                    width="16px"
-                    height="16px"
-                    src="/icon/right-arrow.png"
-                    alt="right"
-                  />
-                </a>
-              </Link>
-            </div>
+            <button
+              className="icon-left"
+              onClick={() => onPrevDay(selectedDayIndex)}
+            >
+              <Image
+                width="20px"
+                height="20px"
+                src="/icon/left-arrow.png"
+                alt="delete"
+              />
+            </button>
+            <div className="day">{days[selectedDayIndex]}</div>
+            <button
+              className="icon-right"
+              onClick={() => onNextDay(selectedDayIndex)}
+            >
+              <Image
+                className="icon"
+                width="20px"
+                height="20px"
+                src="/icon/right-arrow.png"
+                alt="right"
+              />
+            </button>
           </div>
           <form className="form" onSubmit={(e) => handleSubmit(e)}>
             <input
@@ -66,8 +110,13 @@ export default function Home() {
               }}
               value={inputValue}
             />
-            <button type="submit" className="button">
-              <Image width="18px" height="18px" src="/icon/add.png" alt="add" />
+            <button
+              type="submit"
+              className="button"
+              style={{ verticalAlign: "middle" }}
+            >
+              {/* <Image width="18px" height="18px" src="/icon/add.png" alt="add" /> */}
+              <span>submit</span>
             </button>
           </form>
         </div>
@@ -83,29 +132,38 @@ export default function Home() {
                 }}
                 key={index + "_todo"}
               >
-                <div style={{ width: 20, height: 20 }}>
+                <div
+                  onClick={() => onToogleCompletion(index)}
+                  key={index + "_todo"}
+                  className="checked"
+                >
                   <Image
-                    width="20px"
-                    height="20px"
-                    src="/icon/checked.png"
-                    alt="delete"
+                    width="24px"
+                    height="24px"
+                    src="/icon/box.png"
+                    alt="checklist"
                   />
                 </div>
 
-                <div className="label" style={{ padding: "0px 12px" }}>
-                  {todo}
+                <div
+                  className="label"
+                  style={{
+                    padding: "0px 12px",
+                    textDecoration: todo.isDone ? "line-through" : "none",
+                  }}
+                >
+                  {todo.task}
                 </div>
+
                 <div>
                   <button
-                    // style={{ width: 30, height: 30 }}
-                    // type="button"
                     className="delete-button"
                     onClick={() => deleteTodo(index)}
                   >
                     <Image
-                      width="20px"
-                      height="20px"
-                      src="/icon/delete.png"
+                      width="24px"
+                      height="24px"
+                      src="/icon/remove.png"
                       alt="delete"
                     />
                   </button>
@@ -114,18 +172,11 @@ export default function Home() {
             </div>
           );
         })}
-        {/* {todos.map((todo, index) => {
-          return (
-            <div className="footer">
-              <button
-                onClick={() => deleteTodo(index)}
-                className="footer-button"
-              >
-                Clear All
-              </button>
-            </div>
-          );
-        })} */}
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button onClick={() => onClearTodos()} className="button-clear">
+            Clear All
+          </button>
+        </div>
       </div>
     </div>
   );
